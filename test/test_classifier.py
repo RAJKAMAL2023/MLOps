978e07a088bcd15e3e2156097ebcd2c7194c388a
RAJKAMAL2023 <rajgeo91@gmail.com>
2023-08-24 10:51:13 +0000

    adding jupyter files

--- test/test_classifier.py
+++ test/test_classifier.py
@@ -3,8 +3,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 
-# azureml-core of version 1.0.72 or higher is required
-# azureml-dataprep[pandas] of version 1.1.34 or higher is required
 from azureml.core import Workspace, Dataset
 
 subscription_id = '17a98037-fffc-476a-8f95-cae56723b69e'
@@ -15,18 +13,24 @@ workspace = Workspace(subscription_id, resource_group, workspace_name)
 
 dataset = Dataset.get_by_name(workspace, name='Iris')
 df = dataset.to_pandas_dataframe()
-df.head()
-
 
 feature= ['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']
 target = 'Species'
 
-X_train, X_test, y_train, y_test = train_test_split(df[feature], df[target], test_size=0.1, shuffle=True)
+def test_columns():
+    print(df.columns)
+    assert df.columns.to_list() == ['Id','SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm', 'Species']
 
-clf = DecisionTreeClassifier(criterion='entropy')
 
-clf.fit(X_train, y_train)
+def test_classifier_accuracy():
+    X_train, X_test, y_train, y_test = train_test_split(df[feature], df[target], test_size=0.1, shuffle=True)
 
-y_pred = clf.predict(X_test)
+    clf = DecisionTreeClassifier(criterion='entropy')
 
-print(f"The accuracy of the model is {accuracy_score(y_test, y_pred)*100}")
+    clf.fit(X_train, y_train)
+
+    y_pred = clf.predict(X_test)
+
+    print(f"The accuracy of the model is {accuracy_score(y_test, y_pred)*100}")
+
+    assert accuracy_score(y_test, y_pred)*100>90
